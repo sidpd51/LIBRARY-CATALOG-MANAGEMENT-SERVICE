01afced4854ea5c0b0c9385f7ac6e1b82b53ebc3
sidpd51 <sidpd510@gmail.com>
2024-10-15 00:26:01 +0530

    feat: implement comprehensive error handling for all operations in Book

--- src/services/book-service.js
+++ src/services/book-service.js
@@ -1,5 +1,5 @@
 const BookRepository = require("../repositories/book-repository.js");
-
+const { ServiceError } = require("../utils/errors/index.js");
 class BookService {
     constructor() {
         this.bookRepository = new BookRepository();
@@ -10,8 +10,15 @@ class BookService {
             const book = this.bookRepository.createBook(data);
             return book;
         } catch (error) {
-            console.log("something went wrong in service layer");
-            console.log(error);
+            if (
+                error.name == "SequelizeValidationError" ||
+                error.name == "SequelizeUniqueConstraintError" ||
+                error.name == "RepositoryError"
+            ) {
+                throw error;
+            }
+
+            throw new ServiceError();
         }
     }
 
@@ -20,8 +27,10 @@ class BookService {
             const book = this.bookRepository.getBook(bookId);
             return book;
         } catch (error) {
-            console.log("something went wrong in service layer");
-            console.log(error);
+            if (error.name == "RepositoryError") {
+                throw error;
+            }
+            throw new ServiceError();
         }
     }
 
@@ -30,18 +39,25 @@ class BookService {
             const response = this.bookRepository.deleteBook(bookId);
             return response;
         } catch (error) {
-            console.log("something went wrong in service layer");
-            console.log(error);
+            if (error.name == "RepositoryError") {
+                throw error;
+            }
+            throw new ServiceError();
         }
     }
 
-    async updateBook(data,bookId){
+    async updateBook(data, bookId) {
         try {
-            const response = this.bookRepository.updateBook(data,bookId)
+            const response = this.bookRepository.updateBook(data, bookId);
             return response;
         } catch (error) {
-            console.log("something went wrong in service layer");
-            console.log(error);
+            if (
+                error.name == "ValidationError" ||
+                error.name == "RepositoryError"
+            ) {
+                throw error;
+            }
+            throw new ServiceError();
         }
     }
 
@@ -50,8 +66,10 @@ class BookService {
             const books = this.bookRepository.getAllBook(filter);
             return books;
         } catch (error) {
-            console.log("something went wrong in service layer");
-            console.log(error);
+            if (error.name == "RepositoryError") {
+                throw error;
+            }
+            throw new ServiceError();
         }
     }
 }
